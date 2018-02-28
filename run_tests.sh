6dd4cc18345549b608192090f9ba476027fc6a62
sedm0784 <sedm0784@users.noreply.github.com>
2018-02-28 11:31:28 +0000

    Fix plugin paths in run script
    Update to reflect change in my vimconfig: I moved location of all
    bundles from pathogen default into Vim's package area.

--- run_tests.sh
+++ run_tests.sh
@@ -2,8 +2,8 @@
 REAL_HOME=$HOME
 HOME=/dev/null vim -i NONE -c "Vader! *.vader" -Nu <(cat << VIMRC
 filetype off
-set runtimepath+=$REAL_HOME/.vim/bundle/vader
-set runtimepath+=$REAL_HOME/.vim/bundle/vim-list-assist
+set runtimepath+=$REAL_HOME/.vim/pack/bundle/start/vader
+set runtimepath+=$REAL_HOME/.vim/pack/bundle/start/vim-list-assist
 filetype plugin indent on
 syntax enable
 VIMRC)
